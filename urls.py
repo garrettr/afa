c9e992b2e78d6c65d65ed5398822f2b534b82a05
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-07-26 14:50:04 -0700

    Added feincms to urls (AT THE END - that's a catchall regex)

--- urls.py
+++ urls.py
@@ -13,4 +13,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # feincms
+    url(r'', include('feincms.urls')),
+
 )
