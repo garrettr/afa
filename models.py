d1f22d5fd3260bf1070755bf97547c112abde071
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-07-26 16:51:52 -0700

    Built a simple models.py based on the feincms example project

--- models.py
+++ models.py
@@ -3,19 +3,25 @@ from django.utils.translation import ugettext_lazy as _
 from feincms.module.page.models import Page
 from feincms.content.richtext.models import RichTextContent
 from feincms.content.image.models import ImageContent
-
-Page.register_extensions('datepublisher', 'translations') # Example set of extensions
+from feincms.content.raw.models import RawContent
+from feincms.content.medialibrary.models import MediaFileContent
+from feincms.content.application.models import ApplicationContent
+from feincms.content.video.models import VideoContent
 
 Page.register_templates({
-    'title': _('Standard template'),
+    'key': 'base',
+    'title': 'Base Template',
     'path': 'base.html',
     'regions': (
-        ('main', _('Main content area')),
-        ('sidebar', _('Sidebar'), 'inherited'),
+        ('main', 'Main region'),
+        ('sidebar', 'Sidebar', 'inherited'),
         ),
     })
 
 Page.create_content_type(RichTextContent)
+MediaFileContent.default_create_content_type(Page)
+Page.create_content_type(VideoContent)
+Page.create_content_type(RawContent)
 Page.create_content_type(ImageContent, POSITION_CHOICES=(
     ('block', _('block')),
     ('left', _('left')),
