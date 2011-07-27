9a27a264d30ff3fcb6d9932023817ed2cb060291
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-07-26 17:33:57 -0700

    Fix for user-uploaded files.
    Note the serving method added to urls is NOT production-ready.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+import os
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,6 +14,10 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # serve media - DEVELOPMENT ONLY
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), 'media/')}),
 
     # feincms
     url(r'', include('feincms.urls')),
