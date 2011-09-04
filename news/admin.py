08e70fbead53ce3f337a2de6eddc18f7f8c97888
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-09-03 20:07:42 -0400

    Freshen up admin

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from news.models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pub_date', 'headline', 'photo', )
+    list_display_links = ('headline', )
+    search_fields = ('title', 'slug', 'body', )
+    list_per_page = 10
 
 admin.site.register(Post, PostAdmin)
