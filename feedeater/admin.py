403422529e406fa846149cdbf36dc4cf7811cd61
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-11-14 14:53:23 -0500

    Removed unnecessary URL element in list_display (not very useful, as was breaking layout)

--- feedeater/admin.py
+++ feedeater/admin.py
@@ -7,8 +7,7 @@ class FeedAdmin(admin.ModelAdmin):
     list_filter = ('source', )
 
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ('posted_on', 'title', 'feed', 'url', )
-    list_display_links = ('title',)
+    list_display = ('title', 'posted_on', 'feed',)
     search_fields = ('title', 'url', 'description', )
     list_filter = ('feed', 'feed__source',)
     list_per_page = 25
